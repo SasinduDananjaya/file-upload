7644a1aa75022c48cf63aec36f732bf835e6f808
Sasindu Gunawardana <sasindudananjaya2000@gmail.com>
2025-01-11 22:24:31 +0530

    new changes for pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,51 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"React-vite-app",
+    short_name:"react-vite-app",
+    description:"I am a simple vite app",
+    icons:[{
+      src: '/cropped192.jpg',
+      sizes:'192x192',
+      type:'image/jpg',
+      purpose:'favicon'
+    },
+    {
+      src:'/Cjdowner512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/cropped180.jpg',
+      sizes:'180x180',
+      type:'image/jpg',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
