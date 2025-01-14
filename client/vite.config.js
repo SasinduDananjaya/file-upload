916a9d5b2061b8cae7b8c47ea6b39df409d33357
Sasindu Gunawardana <131190393+SasinduDananjaya@users.noreply.github.com>
2025-01-15 00:11:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import path from "path";
+import { fileURLToPath } from 'url';
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const manifestForPlugIn = {
   registerType:'prompt',
